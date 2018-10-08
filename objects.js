e4e36da55dfdbbb7032d242d5c041bf6edccc27f
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-08 21:13:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,12 +7,12 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }
 
