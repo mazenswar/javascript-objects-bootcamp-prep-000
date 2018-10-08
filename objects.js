72952e7f29a7d78b177920b506cdd7cbdcb97c62
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-08 20:58:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,16 @@
 //var meals = {};
 
-var meals = new Object();
+//var meals = new Object();
 
+var playlist = {
+  bobMarley: 'Is this love?',
+}
+
+function updatePlaylist(pl, name, song) {
+  pl.name = song;
+  return pl;
+}
+
+function removeFromPlaylist(pl, name) {
+  delete pl.name;
+}
